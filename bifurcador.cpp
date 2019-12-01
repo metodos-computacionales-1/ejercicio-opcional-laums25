ecb9ba5c2d114ecd6f4460655b92a818103dbc35
laums25 <lm.mercado@uniandes.edu.co>
2019-12-01 04:16:54 +0000

    modificación

--- bifurcador.cpp
+++ bifurcador.cpp
@@ -1,39 +1,38 @@
 #include <iostream>
 
-// Logistic map
-double f(const double r, const double x)
-{
+//Modificado de: https://www.math.utah.edu/~jasonu/code/
+
+double func(const double r, const double x){
   return ( r*x*(1-x) );
 }
 
-double rand_unit()
-{
+double ra(){
   return ( ((double)rand())/RAND_MAX );
 }
 
-int main()
-{
-  const double r_min = 3.0;
-  const double r_max = 4.0;
+int main(){
+  double min = 3.0;
+  double max = 4.0;
   
-  const int num_intervals = 1000;
-  const int max_iterations = 600;
+  int inter = 1000;
+  int maxiter = 600;
 
   srand(time(NULL));
 
-  double r = r_min;
-  double x_n;
+  double r = min;
+  double x;
 
-  for(int i=0; i<num_intervals; ++i){
+  for(int i=0; i<inter; ++i){
 
-    r += (r_max - r_min)/num_intervals;
-    x_n = f(r, rand_unit());
+    r += (max - min)/inter;
+    x = func(r, ra());
 
-    for(int j=0; j<max_iterations; ++j){
-      x_n = f(r, x_n);
-      if(j>=300)	std::cout << r << ' ' << x_n << '\n';
+    for(int j=0; j<maxiter; ++j){
+      x = func(r, x);
+      if(j>=300){	
+       std::cout << r << ' ' << x << '\n';
     }
   }
-  
+  }
   return (0);
 }
